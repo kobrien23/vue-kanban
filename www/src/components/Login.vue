0e6fcf4cad9db7c11dffbb66b9d4cb32f9310a77
kobrien23 <kobrien2345@gmail.com>
2017-02-22 15:18:47 -0700

    changes with login pages

---
+++ www/src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+
+<div class="login-container">
+Hello
+
+<form @submit.prevent="login" >
+email: <input type="email" v-model="email" required>
+password: <input type="password" v-model="password" required><br>
+<button type="submit">submit login</button>
+{{email}}
+
+
+
+
+</form>
+
+
+
+</div>
+
+
+    
+</template>
+
+
+<script>
+export default {
+      name: 'login',
+  data () {
+    return {
+      email: '',
+      password: ''
+
+    }
+  },
+  methods: {
+      login: function(){
+        debugger
+          this.$root.$data.actions.loginUser(this.email, this.password)
+      }
+  }
+}
+
+
+
+
+
+
+</script>
--- www/src/main.js
+++ www/src/main.js
@@ -4,10 +4,56 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+window.axios = axios.create({
+  baseURL: 'https://localhost:3000/api/',
+  timeout: 1000,
+  headers: {'X-Custom-Header': 'foobar'}
+});
+
 /* eslint-disable no-new */
+const store = {
+  state: {
+    user: {},
+    boards: {},
+    board: {},
+
+  },
+  service: { ///asynch activity
+    loginUser: function(email, password) {
+       let user= {
+        email: email,
+        password: password
+      }
+      axios.post('/login/', user)
+    },
+    getUser: function(){
+      return store.state.user
+    },
+    getAuth: function(){
+      return !!store.state.user._id  // this only returns true or false if htey are logged in
+    },
+      registerUser: function(email, password) {
+       let user= {
+        email: email,
+        password: password
+      }
+      axios.post('/register/', user).then(function(res){
+        store.state.user={
+          
+        }
+      })
+    },
+
+
+  },
+}
+
 new Vue({
   el: '#app',
   router,
+  data(){
+    return store
+  },
   template: '<App/>',
   components: { App }
 })
